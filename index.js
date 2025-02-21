28534014a715fc778087730ece5fbe91e9a390a6
TMilanC3 <jmilan@cenero.con>
2025-02-21 12:51:13 -0500

    added Norse font & socials footer

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const masterLibrary = []
 
-const Book = (title, author, year, genre, pages) => {
-    this.title = title
-    this.author = author;
-    this.year = year;
-    this.genre = genre;
-    this.pages = pages;
+class Book {
+    constructor(title, author, year, genre, pages) {
+        this.title = title
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.pages = pages;       
+    }
+    buyANewBook () {
+        masterLibrary.push(book)
+    }
 }
 
-const buyANewBook = (title, author, year, genre, pages) => {
-    const book = new Book(title, author, year, genre, pages)
-    masterLibrary.push(book)
-}
 
 masterLibrary.forEach((book)=>{
     const bookDisplay = document.createElement("div")
-    book = bookDisplay
+    
 })
 
